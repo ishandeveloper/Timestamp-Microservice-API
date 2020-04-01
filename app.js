3ee2891dc02ad29bfba82d15148856514af6c5bd
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-01 23:07:35 +0530

    API

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 var path = require('path');
-var port=process.env.PORT||8080;
+var port = process.env.PORT || 8080;
 
-app.use('/assets',express.static('assets'));
-app.get('/', function(req, res) {
+
+//TimeStamp Function
+const getTimestamp = date => ({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+});
+
+
+app.use('/assets', express.static('assets'));
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
-app.listen(port,() => {
-    console.log("Server Up  → PORT "+port);
+app.get('/api/timestamp',(req,res)=>{
+    let timestamp = getTimestamp(new Date());
+    res.end(JSON.stringify(timestamp));
+});
+
+app.get('/api/timestamp/:dateString',(req,res)=>{
+    const dateString = req.url.split("/api/timestamp/")[1];
+    if (dateString === undefined || dateString.trim() === "") {
+        timestamp = getTimestamp(new Date());
+    } else {
+        const date = !isNaN(dateString) ?
+            new Date(parseInt(dateString)) :
+            new Date(dateString);
+
+        if (!isNaN(date.getTime())) {
+            timestamp = getTimestamp(date);
+        } else {
+            timestamp = {
+                error: "invalid date"
+            };
+        }
+    }
+
+    res.end(JSON.stringify(timestamp));
+});
+
+
+
+
+app.listen(port, () => {
+    console.log("Server Up  → PORT " + port);
 });
