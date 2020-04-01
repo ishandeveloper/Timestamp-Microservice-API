b37e3841a119969e36b09401b2160b9e8710656c
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-01 23:16:32 +0530

    updated documentation

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,10 @@
-require('dotenv').config();
-var path = require('path');
-console.log("hi");
-module.exports=(app)=>{
-        app.get('/',(req,res)=>{
-            console.log("hi");
-            res.sendFile('./views/index.html');
-        });
-};
+// require('dotenv').config();
+// var path = require('path');
+// console.log("hi");
+// module.exports=(app)=>{
+
+//         app.get('/',(req,res)=>{
+//             console.log("hi");
+//             res.sendFile('./views/index.html');
+//         });
+// };
